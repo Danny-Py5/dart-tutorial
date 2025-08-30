51e604d4a48f116a75952e3080a116af15870381
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-08-30 10:51:06 +0100

    create console class for logging

---
+++ console.dart
@@ -0,0 +1,18 @@
+class Console {
+  void log(Object? value) {
+    print("LOG: $value");
+  }
+
+  void info(Object? value) {
+    print("INFO: $value");
+  }
+
+  void warn(Object? value) {
+    print("?? WARNING: $value");
+  }
+
+  void error(Object? value) {
+    print("? ERROR: $value");
+  }
+}
+
