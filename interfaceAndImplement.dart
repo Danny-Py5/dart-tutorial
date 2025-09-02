5255b78ff3bc979ae6fb94d6b85e9bbdb32d7d2b
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-09-02 14:36:06 +0100

    LEARN MORE ON GETTER AND SETTER

--- interfaceAndImplement.dart
+++ interfaceAndImplement.dart
@@ -1,29 +1,56 @@
 abstract class Shape {
-	double area();
-	double peremeter();
-
+  double area();
+  double peremeter();
 }
 
 class Rectangle implements Shape {
-	double length;
-	double breadth;
+  double _length;
+  double _breadth;
 
-	Rectangle(this.length, this.breadth);
+  Rectangle(double length, double breadth)
+    : _length = _validateParameter(length),
+      _breadth = _validateParameter(breadth);
 
+  double get length => _length;
 
+  double get breadth => _breadth;
 
-	@override
-	double area() {
-		return length * breadth;
-	}
+  void set breadth(double value) {
+    if (value >= 0) {
+      _breadth = value;
+    }
+  }
 
-	@override
-	double peremeter(){
-		return (length * 2) + (breadth * 2);
-	}
+  void set length(double value) {
+    if (value >= 0) {
+      _length = value;
+    }
+  }
+
+  static double _validateParameter(double param) {
+    if (param < 0) {
+      throw ArgumentError('$param can be less than 0');
+    } else {
+      return param;
+    }
+  }
+
+  @override
+  double area() {
+    return length * breadth;
+  }
+
+  @override
+  double peremeter() {
+    return (length * 2) + (breadth * 2);
+  }
 }
-void main () {
-	final rec = Rectangle(5, 5);
-	print(rec.area());
-	print(rec.peremeter());
+
+void main() {
+  final rec = Rectangle(5, 95);
+  print(rec.area());
+  print(rec.peremeter());
+
+  rec._length = 4;
+  print(rec.peremeter());
 }
