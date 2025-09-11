103eda874234ede2d3974f5ad2ca58548e0e0d8c
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-09-11 21:39:58 +0100

    read from renters file

--- rental_app/main.dart
+++ rental_app/main.dart
@@ -4,12 +4,12 @@ import '../console.dart';
 
 final console = Console();
 
-void main() {
+Future<void> main() async {
 
 	final user = User('Daniel', 21, 'Geto ago-alaye odigbo local government ondo state Nigeria');
 	
 	final rental = Rental(user);
-	final details = rental.rent('Keyboard');
+	final details = await rental.rent('Keyboard');
 	console.log(details);
 
 }
@@ -25,24 +25,34 @@ class Rental {
 
 
 	 
-	Map<String, dynamic>? rent(String itemName, [int quantity = 1]) {
-		for (Map<String, dynamic> item in itemsDB.getItems()){
+	Future<Map<String, dynamic>>? rent(String itemName, [int quantity = 1]) async {
+		for (Map<String, dynamic> item in await itemsDB.getItems()){
 			for (dynamic value in item.values){
 				String val = value.toString().toLowerCase();
-				if (val == itemName.toLowerCase() && item['isAvailable'] == true){
-					 Map<String, dynamic> rentDetails = {
-						'rent-status': 'success',
-						'rent-date': getDate(),
-						'quantity': quantity,
-						...item,
-						'id': getRandomId(),
-						'renter-info': user.getInfo(),
-					};
-					itemsDB.saveToFIle(rentDetails);
-					return rentDetails;
+				if (val == itemName.toLowerCase()){
+					 if (item['isAvailable'] == true) {
+						 Map<String, dynamic> rentDetails = {
+							'rent-status': 'success',
+							'rent-date': getDate(),
+							'quantity': quantity,
+							...item,
+							'id': getRandomId(),
+							'renter-info': user.getInfo(),
+							'returning-date': getReturningDate(item['range']),
+						};
+						itemsDB.saveToFIle(rentDetails);
+						return rentDetails;
+					 } else {
+					 	return {
+							'rent-status': 'failed',
+							'item': val,
+							'item-is-available': false,
+							'info': 'Item is not available for rent',
+						};
+					 };
 				} 
 			};
-		}
+		};
 		return {};
 	}
 
@@ -58,6 +68,32 @@ class Rental {
 
 	  	return formattedDate;
 	}
+
+	String getReturningDate(String range) {
+		final DateTime now = DateTime.now();
+	  	int day = now.day;
+	  	int month = now.month;
+	  	int year = now.year;
+
+	  	List<dynamic> splitedRange = range.split('-');
+
+	  	switch (splitedRange[1].toLowerCase()){
+	  		case 'day': 
+	  		case 'days': 
+	  			day += int.parse(splitedRange[0]);
+	  			break;
+	  		case 'month':
+			case 'months': 
+	  			month += int.parse(splitedRange[0]);
+	  			break;
+	  	}
+
+	  	// Combine them into the desired format.
+	  	final String formattedDate = '${day.toString().padLeft(2, '0')}/${month.toString().padLeft(2, '0')}/$year';
+
+	  	return formattedDate;
+
+	}
     static String getRandomId() {
 		var strings = 'abcdefghijklmnopqrstuvwxyz1234567890&%#-'.split('');
 		String id = '';
@@ -71,23 +107,43 @@ class Rental {
 
 class ItemsDB {
 
-	List<Map<String, dynamic>> getItems() {
-	 	return [
-	 		{
-	 			'name': 'Guitar',
-	 			'price': 500,
-	 			'range': '1day',
-	 			'id': '1',
-	 			"isAvailable": true,
-	 		}, 
-	 		{
-	 			'name': 'Keyboard',
-	 			'price': 200,
-	 			'range': '1day',
-	 			'id': '2',
-	 			"isAvailable": true,
-	 		}
-	 	];
+	Future<List<Map<String, dynamic>>> parseItems() async {
+		final file = File('itemsDB.txt');
+		late List<Map<String, dynamic>> parsedData;
+		try {
+			if (await file.exists()){
+				final data = await file.readAsString();
+				List<Map<String, dynamic>> dataJson = convert.jsonDecode(data);
+				parsedData = dataJson;
+			} else {
+				return [{'none': 'none'}];
+			}
+		} catch (e) {
+			console.error(e);
+		}
+		console.log(parsedData);
+		return parsedData;
+	}
+
+	Future<List<Map<String, dynamic>>> getItems() async {
+		final items = parseItems();
+		return items;
+	 	// return [
+	 	// 	{
+	 	// 		'name': 'Guitar',
+	 	// 		'price': 500,
+	 	// 		'range': '1-day',
+	 	// 		'id': '1',
+	 	// 		"isAvailable": true,
+	 	// 	}, 
+	 	// 	{
+	 	// 		'name': 'Keyboard',
+	 	// 		'price': 200,
+	 	// 		'range': '1-month',
+	 	// 		'id': '2',
+	 	// 		"isAvailable": true,
+	 	// 	}
+	 	// ];
 	}	
 
 	Future<void> saveToFIle(data) async {
@@ -102,7 +158,7 @@ class ItemsDB {
 				await file.writeAsString(jsonData, mode: FileMode.append);
 			}
 		} catch (e) {
-			console.log(e);
+			console.error(e);
 		}
 	}
 
